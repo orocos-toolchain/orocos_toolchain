182adbec4ddd78f8024670eddec0c0a06dc8bf69
Peter Soetens <peter@thesourceworks.com>
2012-04-09 11:55:11 +0200

    env.sh: added install/bin to PATH.
    Signed-off-by: Peter Soetens <peter@thesourceworks.com>

--- env.sh
+++ env.sh
@@ -9,7 +9,7 @@ if [ x$ROS_ROOT != x ]; then
 ### ROS
 export RUBYLIB=`rospack find utilrb`/lib:`rospack find orogen`/lib:`rosstack find orocos_toolchain`/install/lib/ruby/${RUBY_VERSION}/${RUBY_ARCH}:`rosstack find orocos_toolchain`/install/lib/ruby/${RUBY_VERSION}
 export GEM_HOME=`rosstack find orocos_toolchain`/.gems
-export PATH=`rospack find orogen`/bin:`rosstack find orocos_toolchain`/.gems/bin:$PATH
+export PATH=`rosstack find orocos_toolchain`/install/bin:`rospack find orogen`/bin:`rosstack find orocos_toolchain`/.gems/bin:$PATH
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:`rosstack find orocos_toolchain`/install/lib/pkgconfig
 
 elif [ x${BASH} != x ]; then
@@ -19,14 +19,14 @@ envpath=$PWD
 cd - > /dev/null
 export RUBYLIB=$envpath/utilrb/lib:$envpath/orogen/lib:$envpath/install/lib/ruby/${RUBY_VERSION}/${RUBY_ARCH}:$envpath/install/lib/ruby/${RUBY_VERSION}
 export GEM_HOME=$envpath/.gems
-export PATH=$envpath/orogen/bin:$envpath/.gems/bin:$PATH
+export PATH=$envpath/install/bin:$envpath/orogen/bin:$envpath/.gems/bin:$PATH
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:$envpath/install/lib/pkgconfig
 
 elif [ `basename $PWD` = orocos_toolchain ]; then
 ### non-Bash, non-ROS
 export RUBYLIB=$PWD/utilrb/lib:$PWD/orogen/lib:$PWD/install/lib/ruby/${RUBY_VERSION}/${RUBY_ARCH}:$PWD/install/lib/ruby/${RUBY_VERSION}
 export GEM_HOME=$PWD/.gems
-export PATH=$PWD/orogen/bin:$PWD/.gems/bin:$PATH
+export PATH=$PWD/install/bin:$PWD/orogen/bin:$PWD/.gems/bin:$PATH
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:$PWD/install/lib/pkgconfig
 else
   echo "Error: This script must be sourced from the 'orocos_toolchain' directory when not running in a ROS_ROOT nor bash environment."
