22a313d48c55660c87c75905a398c5ccd15add28
Ruben Smits <ruben.smits@intermodalics.eu>
2013-12-05 11:06:04 +0100

    finalize catkinization of orocos_toolchain
    Signed-off-by: Ruben Smits <ruben.smits@intermodalics.eu>

--- env.mk
+++
@@ -1,7 +0,0 @@
-# This file is ROS specific
-export RUBYLIB:=$(shell rospack find utilrb)/lib:$(shell rospack find orogen)/lib:$(shell rospack find typelib)/bindings/ruby/lib:$(shell rospack find typelib)/lib
-export GEM_HOME:=$(shell rosstack find orocos_toolchain)/.gems
-export RUBYOPT:=-rubygems
-export PATH:=$(shell rospack find orogen)/bin:$(shell rosstack find orocos_toolchain)/.gems/bin:${PATH}
-export TYPELIB_USE_GCCXML:=1
-export PKG_CONFIG_PATH=$(shell rosstack find orocos_toolchain)/install/lib/pkgconfig
